0238704391c0fda1450e631e5db4ea90cc70f1b1
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-02-10 11:33:50 -0800

    create new contact form in homepage.html.twig

--- app/app.php
+++ app/app.php
@@ -1,16 +1,21 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/tamagotchi.php";
-    // require_once __DIR__."/../css/styles.css";
+    require_once __DIR__."/../src/adress_book.php";
+
     session_start();
-    if (empty($_SESSION['tama'])) {
-     $_SESSION['tama'] = array();
+
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
     }
+
     $app = new Silex\Application();
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-     'twig.path' => __DIR__.'/../views'
+        'twig.path' => __DIR__.'/../views'
     ));
+
+    $app['debug'] = true;
 
 
 
