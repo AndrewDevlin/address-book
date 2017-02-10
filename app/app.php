e3504577f423498b366d25b3cb2e9c15efa44233
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-02-10 14:47:31 -0800

    instantiate new object and show on confirmation page

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/adress_book.php";
+    require_once __DIR__."/../src/address_book.php";
 
     session_start();
 
@@ -16,6 +16,18 @@
     ));
 
     $app['debug'] = true;
+
+    // when localhost:8000 is searched send to homepage
+    $$app->get('/', function() use ($app) {
+        return $app['twig']->render('homepage.html.twig');
+    });
+
+    // instantiate new object and send to confirmation page
+    $app->post('/add_contact', function() use ($app) {
+        $new_contact = new Contact($_POST["set_first_name"], $_POST["set_last_name"], $_POST["set_phone_number"], $_POST["set_street"], $_POST["set_city"], $_POST["set_state"], $_POST["set_zip"]);
+        $new_contact->save();
+        return $app['twig']->render('create_contact.html.twig', array('new_contact' => $new_contact));
+    });
 
 
 
